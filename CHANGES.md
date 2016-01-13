4231891c09f369a9b97195b4fbe22ef06f167a3b
TB026891 <tim.baxter@cerner.com>
2016-01-13 12:16:06 -0600

    updates for 1.8

--- CHANGES.md
+++ CHANGES.md
@@ -1,4 +1,7 @@
 # Autotagger Change Log
 
+## 0.2.0
+* Updates for Django 1.8+ and some PEP-8 cleanup.
+
 ## 0.1
 * Initial release.
--- autotagger/models.py
+++ autotagger/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
 
@@ -10,16 +10,17 @@ class AutoTag(models.Model):
     Via autotagging, it will establish relationships between content
     matching the given phrase.
     """
-    phrase   = models.CharField(max_length=200, help_text="""
+    phrase = models.CharField(max_length=200, help_text="""
                     The phrase you want to search for.
                     IMPORTANT: be sure your phrase won't create false positives.
-                    For example, Attempting to tag 'Django' would also tag 'Django Reinhardt', 'Django Unchained',
+                    For example, Attempting to tag 'Django' would also tag
+                    'Django Reinhardt', 'Django Unchained',
                     and any other instance of the phrase.
                     """)
-    articles       = models.ManyToManyField('articles.Article', blank=True, null=True, editable=False)
-    content_type   = models.ForeignKey(ContentType, blank=True, null=True)
-    object_id      = models.PositiveIntegerField(null=True, blank=True)
-    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    articles = models.ManyToManyField('articles.Article', blank=True, null=True, editable=False)
+    content_type = models.ForeignKey(ContentType, blank=True, null=True)
+    object_id = models.PositiveIntegerField(null=True, blank=True)
+    content_object = GenericForeignKey('content_type', 'object_id')
 
     def __unicode__(self):
         return self.phrase
